4671b6b71ee5193a719aae4ea1ed5b89bce05ea7
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2020-05-17 17:52:55 +0200

    Added record audio via mic feature + Fixed frame division bug

--- audio_manager.py
+++ audio_manager.py
@@ -1,7 +1,8 @@
 import numpy as np
 import librosa
-from scipy.signal import butter, sosfilt, correlate
+from scipy.signal import butter, sosfilt
 from scipy.io import wavfile
+import sounddevice as sd
 
 
 def butter_bandpass(lowcut, highcut, sample_frequency, order=5):
@@ -34,8 +35,28 @@ def get_from_file(input_file_path):
     return librosa.load(input_file_path, sr=None)
 
 
-def get_from_mic(buffer_size=256, samplerate=44100):
-    return None
+def get_from_mic(buffer_size=1024, samplerate=44100):
+    try:
+        sd.default.samplerate = samplerate
+        sd.default.dtype = np.float32
+        sd.default.channels = 1
+        sd.default.blocksize = buffer_size
+
+        recording = []
+
+        def callback(indata, outdata, frames, time, status):
+            if status:
+                print(status)
+
+            for i in range(0, len(indata)):
+                recording.extend(indata[i])
+
+        with sd.Stream(callback=callback):
+            input("Press any key to stop the recording ...")
+            return np.asarray(recording)
+
+    except Exception as e:
+        print(e)
 
 
 def retrieve_audio(input_file_path=''):
@@ -45,8 +66,12 @@ def retrieve_audio(input_file_path=''):
         return get_from_file(input_file_path)
 
 
-def chop_in_frames(signal, frame_number):
-    return np.split(signal, frame_number)
+def chop_in_frames(signal, frame_size):
+
+    while len(signal) % frame_size != 0:
+        signal = np.append(signal, 0)
+
+    return np.split(signal, len(signal) / frame_size)
 
 
 def extract_features(sample_rate, signal):
--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import audio_manager as audio
 import matplotlib.pyplot as plt
 import librosa.display
 
+sample_rate = 44100
+
 # Get signal from file (or no parameter for mic)
-signal, sample_rate = audio.retrieve_audio('in.wav')
+# signal, sample_rate = audio.retrieve_audio('in.wav')
+signal = audio.retrieve_audio()
 
 # Apply Filtering
 processed_signal = audio.process(signal, [200, 500, 1000, 5000], sample_rate)
@@ -12,21 +15,21 @@ processed_signal = audio.process(signal, [200, 500, 1000, 5000], sample_rate)
 # Save processed Signal (OPTIONAL)
 audio.save_signal('out.wav', processed_signal, sample_rate)
 
-# # Chop in frames
-# frames = audio.chop_in_frames(processed_signal, 3)
-#
-# # Extract Features
-# features = []
-# for i in range(0, len(frames)):
-#     features.append(audio.extract_features(sample_rate, frames[i]))
-#
-# # Print Results
-# print("Sample Rate : ", sample_rate, "Hz")
-# print("Signal Size : ", len(signal), "samples")
-# print("Signal Time : ", len(signal) / sample_rate, "seconds")
-# print("NB Frames   : ", len(frames))
-# print("Frame size  : ", len(frames[0]), "samples")
-# print("Frame time  : ", len(frames[0]) / sample_rate, "seconds")
-#
-# librosa.display.specshow(features[0], sr=sample_rate, x_axis='time')
-# plt.show()
+# Chop in frames
+frames = audio.chop_in_frames(processed_signal, 11025)
+
+# Extract Features
+features = []
+for i in range(0, len(frames)):
+    features.append(audio.extract_features(sample_rate, frames[i]))
+
+# Print Results
+print("Sample Rate : ", sample_rate, "Hz")
+print("Raw Signal Size : ", len(signal), "samples")
+print("Raw Signal Time : ", len(signal) / sample_rate, "seconds")
+print("NB Frames   : ", len(frames))
+print("Frame size  : ", len(frames[0]), "samples")
+print("Frame time  : ", len(frames[0]) / sample_rate, "seconds")
+
+librosa.display.specshow(features[0], sr=sample_rate, x_axis='time')
+plt.show()
