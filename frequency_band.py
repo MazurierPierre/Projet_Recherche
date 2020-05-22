a17a4b9bd9c094bc6dd0815a2bc28450bdc113b1
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2020-05-22 17:38:29 +0200

    Added frequencBand class

---
+++ frequency_band.py
@@ -0,0 +1,9 @@
+class FrequencyBand:
+    frequency = 100
+    bandwidth = 50
+    order = 3
+
+    def __init__(self, frequency, bandwidth, order):
+        self.frequency = frequency
+        self.bandwidth = bandwidth
+        self.order = order
